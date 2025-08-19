f6386ad632fe840a2683d1d050d448354b44d3c2
Victor Bradford <ablackweb@gmail.com>
2025-08-19 12:56:54 -0400

    ES: use Spanish course cards on /es and add CourseCardsEs

--- src/app/es/page.tsx
+++ src/app/es/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import CourseCards from "../components/CourseCards";
+import CourseCardsEs from "../components/CourseCardsEs";
 
 export default function HomeEs() {
   return (
